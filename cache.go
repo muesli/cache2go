841e5f69ffd979e8d78c031a72a47edce708fc74
Radu Ioan Fericean <radu@fericean.ro>
2012-08-14 18:46:12 +0300

    added license and some docs

--- cache.go
+++ cache.go
@@ -1,21 +1,4 @@
-/*
-Rating system designed to be used in VoIP Carriers World
-Copyright (C) 2012  Radu Ioan Fericean
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>
-*/
-
+//Simple caching library with expiration capabilities
 package cache
 
 import (
@@ -26,15 +9,16 @@ import (
 
 type expiringCacheEntry interface {
 	XCache(key string, expire time.Duration, value expiringCacheEntry)
-	Expire()
 	KeepAlive()
 }
 
+// Structure that must be embeded in the objectst that must be cached with expiration.
+// If the expiration is not needed this can be ignored
 type XEntry struct {
 	sync.Mutex
-	key       string
-	keepAlive bool
-	expire    time.Duration
+	key            string
+	keepAlive      bool
+	expireDuration time.Duration
 }
 
 var (
@@ -42,20 +26,22 @@ var (
 	cache  = make(map[string]interface{})
 )
 
+// The main function to cache with expiration
 func (xe *XEntry) XCache(key string, expire time.Duration, value expiringCacheEntry) {
 	xe.keepAlive = true
 	xe.key = key
-	xe.expire = expire
+	xe.expireDuration = expire
 	xcache[key] = value
-	go xe.Expire()
+	go xe.expire()
 }
 
-func (xe *XEntry) Expire() {
+// The internal mechanism for expiartion
+func (xe *XEntry) expire() {
 	for xe.keepAlive {
 		xe.Lock()
 		xe.keepAlive = false
 		xe.Unlock()
-		t := time.NewTimer(xe.expire)
+		t := time.NewTimer(xe.expireDuration)
 		<-t.C
 		xe.Lock()
 		if !xe.keepAlive {
@@ -65,12 +51,14 @@ func (xe *XEntry) Expire() {
 	}
 }
 
+// Mark entry to be kept another expirationDuration period
 func (xe *XEntry) KeepAlive() {
 	xe.Lock()
 	defer xe.Unlock()
 	xe.keepAlive = true
 }
 
+// Get an entry from the expiration cache and mark it for keeping alive
 func GetXCached(key string) (ece expiringCacheEntry, err error) {
 	if r, ok := xcache[key]; ok {
 		r.KeepAlive()
@@ -79,10 +67,12 @@ func GetXCached(key string) (ece expiringCacheEntry, err error) {
 	return nil, errors.New("not found")
 }
 
+// The function to be used to cache a key/value pair when expiration is not needed
 func Cache(key string, value interface{}) {
 	cache[key] = value
 }
 
+// The function to extract a value for a key that never expire
 func GetCached(key string) (v interface{}, err error) {
 	if r, ok := cache[key]; ok {
 		return r, nil
