24c9a5f7ca397ade07de085b64c3217ad09779a5
Christian Muehlhaeuser <muesli@gmail.com>
2013-11-11 15:10:37 +0100

    * Prevent dead-lock and renamed vars a bit.

--- cache.go
+++ cache.go
@@ -12,8 +12,9 @@ import (
 type CacheEntry struct {
 	sync.Mutex
 	key            string
-	expireDuration time.Duration
-	expiringSince  time.Time
+	lifeSpan       time.Duration
+	createdOn      time.Time
+	accessedOn     time.Time
 	data           interface{}
 
 	// Callback method triggered right before removing the item from the cache
@@ -23,10 +24,10 @@ type CacheEntry struct {
 // Structure of a table with items in the cache
 type CacheTable struct {
 	sync.RWMutex
-	name        string
-	items       map[string]*CacheEntry
-	expTimer    *time.Timer
-	expDuration time.Duration
+	name            string
+	items           map[string]*CacheEntry
+	cleanupTimer    *time.Timer
+	cleanupInterval time.Duration
 }
 
 var (
@@ -38,21 +39,28 @@ var (
 func (entry *CacheEntry) KeepAlive() {
 	entry.Lock()
 	defer entry.Unlock()
-	entry.expiringSince = time.Now()
+	entry.accessedOn = time.Now()
 }
 
 // Returns this entry's expiration duration
-func (entry *CacheEntry) ExpireDuration() time.Duration {
+func (entry *CacheEntry) LifeSpan() time.Duration {
 	entry.Lock()
 	defer entry.Unlock()
-	return entry.expireDuration
+	return entry.lifeSpan
 }
 
-// Returns since when this entry is expiring
-func (entry *CacheEntry) ExpiringSince() time.Time {
+// Returns when this entry was last accessed
+func (entry *CacheEntry) AccessedOn() time.Time {
 	entry.Lock()
 	defer entry.Unlock()
-	return entry.expiringSince
+	return entry.accessedOn
+}
+
+// Returns when this entry was added to the cache
+func (entry *CacheEntry) CreatedOn() time.Time {
+	entry.Lock()
+	defer entry.Unlock()
+	return entry.createdOn
 }
 
 // Returns the value of this cached item
@@ -93,8 +101,8 @@ func (table *CacheTable) CacheCount() int {
 // Expiration check loop, triggered by a self-adjusting timer
 func (table *CacheTable) expirationCheck() {
 	table.Lock()
-	if table.expTimer != nil {
-		table.expTimer.Stop()
+	if table.cleanupTimer != nil {
+		table.cleanupTimer.Stop()
 	}
 
 	// Take a copy of cache so we can iterate over it without blocking the mutex
@@ -106,52 +114,58 @@ func (table *CacheTable) expirationCheck() {
 	now := time.Now()
 	smallestDuration := 0 * time.Second
 	for key, c := range cc {
-		if c.expireDuration == 0 {
+		if c.lifeSpan == 0 {
 			continue
 		}
-		if now.Sub(c.expiringSince) >= c.expireDuration {
-			table.Lock()
+		if now.Sub(c.accessedOn) >= c.lifeSpan {
 			if c.aboutToExpire != nil {
 				c.aboutToExpire(key)
 			}
+			table.Lock()
 			delete(table.items, key)
 			table.Unlock()
 		} else {
-			if smallestDuration == 0 || c.ExpireDuration() < smallestDuration {
-				smallestDuration = c.ExpireDuration() - now.Sub(c.ExpiringSince())
+			if smallestDuration == 0 || c.lifeSpan < smallestDuration {
+				smallestDuration = c.lifeSpan - now.Sub(c.accessedOn)
 			}
 		}
 	}
 
 	table.Lock()
-	table.expDuration = smallestDuration
+	table.cleanupInterval = smallestDuration
 	if smallestDuration > 0 {
-		table.expTimer = time.AfterFunc(smallestDuration, func() {
+		table.cleanupTimer = time.AfterFunc(smallestDuration, func() {
 			go table.expirationCheck()
 		})
 	}
 	table.Unlock()
 }
 
-// Adds a key/value pair to the cache
-// The last parameter abouToExpireFunc can be nil. Otherwise abouToExpireFunc
-// will be called (with this item's key as its only parameter), right before
-// removing this item from the cache
-func (table *CacheTable) Cache(key string, expire time.Duration, data interface{}, aboutToExpireFunc func(string)) {
+/* Adds a key/value pair to the cache
+ / key is a unique cache-item key in the cache
+ / lifeSpan indicates how long this item will remain in the cache after its
+ / last access
+ / data is the cache-item value
+ / The last parameter abouToExpireFunc can be nil. Otherwise abouToExpireFunc
+ / will be called (with this item's key as its only parameter), right before
+ / removing this item from the cache
+*/
+func (table *CacheTable) Cache(key string, lifeSpan time.Duration, data interface{}, aboutToExpireFunc func(string)) {
 	entry := CacheEntry{}
 	entry.key = key
-	entry.expireDuration = expire
-	entry.expiringSince = time.Now()
+	entry.lifeSpan = lifeSpan
+	entry.createdOn = time.Now()
+	entry.accessedOn = entry.createdOn
 	entry.aboutToExpire = aboutToExpireFunc
 	entry.data = data
 
 	table.Lock()
 	table.items[key] = &entry
-	expDur := table.expDuration
+	expDur := table.cleanupInterval
 	table.Unlock()
 
 	// If we haven't set up any expiration check timer or found a more imminent item
-	if expire > 0 && (expDur == 0 || expire < expDur) {
+	if lifeSpan > 0 && (expDur == 0 || lifeSpan < expDur) {
 		table.expirationCheck()
 	}
 }
@@ -173,9 +187,9 @@ func (table *CacheTable) Flush() {
 	defer table.Unlock()
 
 	table.items = make(map[string]*CacheEntry)
-	table.expDuration = 0
-	if table.expTimer != nil {
-		table.expTimer.Stop()
+	table.cleanupInterval = 0
+	if table.cleanupTimer != nil {
+		table.cleanupTimer.Stop()
 	}
 
 	mutex.Lock()
