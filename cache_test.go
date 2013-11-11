8b383c863125a570d5f0041166ece7d8fc3a1e3d
Christian Muehlhaeuser <muesli@gmail.com>
2013-11-11 12:01:38 +0100

    * Rewritten tests to support cache tables.

--- cache_test.go
+++ cache_test.go
@@ -4,79 +4,80 @@ import (
 	"strconv"
 	"testing"
 	"time"
-	_ "fmt"
+	"fmt"
 )
 
-type myStruct struct {
-	XEntry
-	data string
-}
-
 func TestCache(t *testing.T) {
-	a := &myStruct{data: "testy test"}
-	a.XCache("test", 1*time.Second, a, nil)
-	b, err := GetXCached("test")
-	if err != nil || b == nil || b != a {
+	table := CacheTable("table")
+	a := "testy test"
+	table.XCache("test", 1*time.Second, a, nil)
+	b, err := table.GetXCached("test")
+	if err != nil || b == nil || b.Data().(string) != a {
 		t.Error("Error retrieving data from cache", err)
 	}
 }
 
 func TestCacheExpire(t *testing.T) {
-	a := &myStruct{data: "testy test"}
-	a.XCache("test", 1*time.Second, a, nil)
-	b, err := GetXCached("test")
-	if err != nil || b == nil || b.(*myStruct).data != "testy test" {
+	table := CacheTable("table")
+	a := "testy test"
+	table.XCache("test", 1*time.Second, a, nil)
+	b, err := table.GetXCached("test")
+	if err != nil || b == nil || b.Data().(string) != a {
 		t.Error("Error retrieving data from cache", err)
 	}
 	time.Sleep(1500 * time.Millisecond)
-	b, err = GetXCached("test")
+	b, err = table.GetXCached("test")
 	if err == nil || b != nil {
 		t.Error("Error expiring data")
 	}
 }
 
 func TestCacheKeepAlive(t *testing.T) {
-	a := &myStruct{data: "testy test"}
-	a.XCache("test", 500*time.Millisecond, a, nil)
-	a = &myStruct{data: "testest test"}
-	a.XCache("test2", 1250*time.Millisecond, a, nil)
-	b, err := GetXCached("test")
-	if err != nil || b == nil || b.(*myStruct).data != "testy test" {
+	table := CacheTable("table")
+	a := "testy test"
+	table.XCache("test", 500*time.Millisecond, a, nil)
+	a = "testest test"
+	table.XCache("test2", 1250*time.Millisecond, a, nil)
+	b, err := table.GetXCached("test")
+	if err != nil || b == nil || b.Data().(string) != "testy test" {
 		t.Error("Error retrieving data from cache", err)
 	}
 	time.Sleep(200 * time.Millisecond)
 	b.KeepAlive()
 	time.Sleep(750 * time.Millisecond)
-	b, err = GetXCached("test")
+	b, err = table.GetXCached("test")
 	if err == nil || b != nil {
 		t.Error("Error expiring data")
 	}
-	b, err = GetXCached("test2")
-	if err != nil || b == nil || b.(*myStruct).data != "testest test" {
+	b, err = table.GetXCached("test2")
+	if err != nil || b == nil || b.Data().(string) != "testest test" {
 		t.Error("Error retrieving data from cache", err)
 	}
 	time.Sleep(1500 * time.Millisecond)
-	b, err = GetXCached("test2")
+	b, err = table.GetXCached("test2")
 	if err == nil || b != nil {
 		t.Error("Error expiring data")
 	}
 }
 
 func TestFlush(t *testing.T) {
-	a := &myStruct{data: "testy test"}
-	a.XCache("test", 10*time.Second, a, nil)
-	XFlush()
-	b, err := GetXCached("test")
+	table := CacheTable("table")
+	a := "testy test"
+	table.XCache("test", 10*time.Second, a, nil)
+	time.Sleep(1000 * time.Millisecond)
+	table.XFlush()
+	b, err := table.GetXCached("test")
 	if err == nil || b != nil {
 		t.Error("Error expiring data")
 	}
 }
 
 func TestFlushNoTimout(t *testing.T) {
-	a := &myStruct{data: "testy test"}
-	a.XCache("test", 10*time.Second, a, nil)
-	XFlush()
-	b, err := GetXCached("test")
+	table := CacheTable("table")
+	a := "testy test"
+	table.XCache("test", 10*time.Second, a, nil)
+	table.XFlush()
+	b, err := table.GetXCached("test")
 	if err == nil || b != nil {
 		t.Error("Error expiring data")
 	}
@@ -85,15 +86,24 @@ func TestFlushNoTimout(t *testing.T) {
 func TestMassive(t *testing.T) {
 	return
 
-	for i := 0; i < 1000000; i++ {
-		a := &myStruct{data: "testy test"}
+	table := CacheTable("table")
+	val := "testy test"
+	for i := 0; i < 100000; i++ {
 		key := "test_" + strconv.Itoa(i)
-		a.XCache(key, 1*time.Second, a, nil)
+		table.XCache(key, 1*time.Second, val, nil)
+	}
+	for i := 0; i < 100000; i++ {
+		key := "test_" + strconv.Itoa(i)
+		d, err := table.GetXCached(key)
+		if err != nil || d == nil || d.Data().(string) != val {
+			t.Error("Error retrieving data")
+			panic(err)
+		}
 	}
 
-//	fmt.Println("In Cache:", XCacheCount())
-	time.Sleep(500 * time.Millisecond)
-//	fmt.Println("In Cache:", XCacheCount())
+	fmt.Println("In Cache:", table.XCacheCount())
+	time.Sleep(1000 * time.Millisecond)
+	fmt.Println("In Cache:", table.XCacheCount())
 	time.Sleep(1500 * time.Millisecond)
-//	fmt.Println("In Cache:", XCacheCount())
+	fmt.Println("In Cache:", table.XCacheCount())
 }
