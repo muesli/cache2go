f42e636825760f33a08939c8e298349901475819
Christian Muehlhaeuser <muesli@gmail.com>
2013-11-12 13:14:32 +0100

    * Be a godoc conformist.

--- cache.go
+++ cache.go
@@ -15,16 +15,24 @@ import (
 	"time"
 )
 
-// Structure of an item in the cache
-// data contains the user-set value in the cache
+/* Structure of an item in the cache.
+ *	Parameter data contains the user-set value in the cache.
+ */
 type CacheItem struct {
 	sync.RWMutex
+
+	// The item's key
 	key      interface{}
+	// The item's data
 	data     interface{}
+	// How long will the item live in the cache when not being accessed/kept alive
 	lifeSpan time.Duration
 
+	// Creation timestamp
 	createdOn   time.Time
+	// Last access timestamp
 	accessedOn  time.Time
+	// How often the item was accessed
 	accessCount int64
 
 	// Callback method triggered right before removing the item from the cache
@@ -34,11 +42,18 @@ type CacheItem struct {
 // Structure of a table with items in the cache
 type CacheTable struct {
 	sync.RWMutex
+
+	// The table's name
 	name            string
+	// All cached items
 	items           map[interface{}]*CacheItem
+
+	// Timer responsible for triggering cleanup
 	cleanupTimer    *time.Timer
+	// Last used timer duration
 	cleanupInterval time.Duration
 
+	// The logger used for this table
 	logger *log.Logger
 
 	// Callback method triggered when trying to load a non-existing key
@@ -54,7 +69,12 @@ var (
 	mutex sync.RWMutex
 )
 
-// Returns a newly created CacheItem
+/* Returns a newly created CacheItem.
+ *	Parameter key is the item's cache-key.
+ *	Parameter lifeSpan determines after which time period without an access the item
+ *		will get removed from the cache.
+ *	Parameter data is the item's value.
+ */
 func CreateCacheItem(key interface{}, lifeSpan time.Duration, data interface{}) CacheItem {
 	t := time.Now()
 	return CacheItem{
@@ -68,7 +88,7 @@ func CreateCacheItem(key interface{}, lifeSpan time.Duration, data interface{}) 
 	}
 }
 
-// Mark item to be kept for another expireDuration period
+// Mark item to be kept for another expireDuration period.
 func (item *CacheItem) KeepAlive() {
 	item.Lock()
 	defer item.Unlock()
@@ -76,46 +96,46 @@ func (item *CacheItem) KeepAlive() {
 	item.accessCount++
 }
 
-// Returns this item's expiration duration
+// Returns this item's expiration duration.
 func (item *CacheItem) LifeSpan() time.Duration {
 	// immutable
 	return item.lifeSpan
 }
 
-// Returns when this item was last accessed
+// Returns when this item was last accessed.
 func (item *CacheItem) AccessedOn() time.Time {
 	item.RLock()
 	defer item.RUnlock()
 	return item.accessedOn
 }
 
-// Returns when this item was added to the cache
+// Returns when this item was added to the cache.
 func (item *CacheItem) CreatedOn() time.Time {
 	// immutable
 	return item.createdOn
 }
 
-// Returns how often this item has been accessed
+// Returns how often this item has been accessed.
 func (item *CacheItem) AccessCount() int64 {
 	item.RLock()
 	defer item.RUnlock()
 	return item.accessCount
 }
 
-// Returns the key of this cached item
+// Returns the key of this cached item.
 func (item *CacheItem) Key() interface{} {
 	// immutable
 	return item.key
 }
 
-// Returns the value of this cached item
+// Returns the value of this cached item.
 func (item *CacheItem) Data() interface{} {
 	// immutable
 	return item.data
 }
 
 // Configures a callback, which will be called right before the item
-// is about to be removed from the cache
+// is about to be removed from the cache.
 func (item *CacheItem) SetAboutToExpireCallback(f func(interface{})) {
 	item.Lock()
 	defer item.Unlock()
@@ -123,7 +143,7 @@ func (item *CacheItem) SetAboutToExpireCallback(f func(interface{})) {
 }
 
 // Returns the existing cache table with given name or creates a new one
-// if the table does not exist yet
+// if the table does not exist yet.
 func Cache(table string) *CacheTable {
 	mutex.RLock()
 	t, ok := cache[table]
@@ -143,7 +163,7 @@ func Cache(table string) *CacheTable {
 	return t
 }
 
-// Returns how many items are currently stored in the cache
+// Returns how many items are currently stored in the cache.
 func (table *CacheTable) Count() int {
 	table.RLock()
 	defer table.RUnlock()
@@ -152,7 +172,7 @@ func (table *CacheTable) Count() int {
 }
 
 // Configures a data-loader callback, which will be called when trying
-// to use access a non-existing key
+// to use access a non-existing key.
 func (table *CacheTable) SetDataLoader(f func(interface{}) *CacheItem) {
 	table.Lock()
 	defer table.Unlock()
@@ -160,7 +180,7 @@ func (table *CacheTable) SetDataLoader(f func(interface{}) *CacheItem) {
 }
 
 // Configures a callback, which will be called every time a new item
-// is added to the cache
+// is added to the cache.
 func (table *CacheTable) SetAddedItemCallback(f func(*CacheItem)) {
 	table.Lock()
 	defer table.Unlock()
@@ -168,21 +188,21 @@ func (table *CacheTable) SetAddedItemCallback(f func(*CacheItem)) {
 }
 
 // Configures a callback, which will be called every time an item
-// is about to be removed from the cache
+// is about to be removed from the cache.
 func (table *CacheTable) SetAboutToDeleteItemCallback(f func(*CacheItem)) {
 	table.Lock()
 	defer table.Unlock()
 	table.aboutToDeleteItem = f
 }
 
-// Sets the logger to be used by this cache table
+// Sets the logger to be used by this cache table.
 func (table *CacheTable) SetLogger(logger *log.Logger) {
 	table.Lock()
 	defer table.Unlock()
 	table.logger = logger
 }
 
-// Expiration check loop, triggered by a self-adjusting timer
+// Expiration check loop, triggered by a self-adjusting timer.
 func (table *CacheTable) expirationCheck() {
 	table.Lock()
 	if table.cleanupInterval > 0 {
@@ -194,7 +214,7 @@ func (table *CacheTable) expirationCheck() {
 		table.cleanupTimer.Stop()
 	}
 
-	// Take a copy of cache so we can iterate over it without blocking the mutex
+	// Take a copy of cache so we can iterate over it without blocking the mutex.
 	cc := table.items
 	table.Unlock()
 
@@ -230,10 +250,11 @@ func (table *CacheTable) expirationCheck() {
 	table.Unlock()
 }
 
-/* Adds a key/value pair to the cache
-   key is a unique item-key in the cache
-   lifeSpan indicates how long this item will remain in cache after its last access
-   data is the item's value
+/* Adds a key/value pair to the cache.
+ *	Parameter key is the item's cache-key.
+ *	Parameter lifeSpan determines after which time period without an access the item
+ *		will get removed from the cache.
+ *	Parameter data is the item's value.
  */
 func (table *CacheTable) Cache(key interface{}, lifeSpan time.Duration, data interface{}) *CacheItem {
 	item := CreateCacheItem(key, lifeSpan, data)
@@ -257,7 +278,7 @@ func (table *CacheTable) Cache(key interface{}, lifeSpan time.Duration, data int
 	return &item
 }
 
-// Delete an item from the cache
+// Delete an item from the cache.
 func (table *CacheTable) Delete(key interface{}) (*CacheItem, error) {
 	table.RLock()
 	r, ok := table.items[key]
@@ -267,7 +288,7 @@ func (table *CacheTable) Delete(key interface{}) (*CacheItem, error) {
 		return nil, errors.New("Key not found in cache")
 	}
 
-	// Trigger callbacks before deleting an item from cache
+	// Trigger callbacks before deleting an item from cache.
 	if table.aboutToDeleteItem != nil {
 		table.aboutToDeleteItem(r)
 	}
@@ -287,7 +308,8 @@ func (table *CacheTable) Delete(key interface{}) (*CacheItem, error) {
 }
 
 // Test whether an item exists in the cache. Unlike the Value method
-// Exists never tries to fetch data via the loadData callback
+// Exists neither tries to fetch data via the loadData callback nor
+// does it keep the item alive in the cache.
 func (table *CacheTable) Exists(key interface{}) bool {
 	table.RLock()
 	defer table.RUnlock()
@@ -296,7 +318,7 @@ func (table *CacheTable) Exists(key interface{}) bool {
 	return ok
 }
 
-// Get an item from the cache and mark it to be kept alive
+// Get an item from the cache and mark it to be kept alive.
 func (table *CacheTable) Value(key interface{}) (*CacheItem, error) {
 	table.RLock()
 	r, ok := table.items[key]
@@ -320,7 +342,7 @@ func (table *CacheTable) Value(key interface{}) (*CacheItem, error) {
 	return nil, errors.New("Key not found in cache")
 }
 
-// Delete all items from cache
+// Delete all items from cache.
 func (table *CacheTable) Flush() {
 	table.Lock()
 	defer table.Unlock()
@@ -334,7 +356,7 @@ func (table *CacheTable) Flush() {
 	}
 }
 
-// Internal logging method for convenience
+// Internal logging method for convenience.
 func (table *CacheTable) log(v ...interface{}) {
 	if table.logger == nil {
 		return
