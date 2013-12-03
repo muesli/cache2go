6ceeabf08de2f5878bea17ce98dcde7d63330d6d
Christian Muehlhaeuser <muesli@gmail.com>
2013-12-03 17:12:58 +0100

    * Sorting was reverted.

--- cachetable.go
+++ cachetable.go
@@ -254,7 +254,7 @@ type Pair struct {
 type PairList []Pair
 func (p PairList) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
 func (p PairList) Len() int { return len(p) }
-func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+func (p PairList) Less(i, j int) bool { return p[i].Value > p[j].Value }
 
 func (table *CacheTable) MostAccessed(count int64) []interface{} {
 	table.RLock()
