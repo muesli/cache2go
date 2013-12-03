f61c3d33f62337185c84ad9593e2ff8bed77e5f6
Christian Muehlhaeuser <muesli@gmail.com>
2013-12-03 21:10:08 +0100

    * Less generic names for sort helper structs.

--- cachetable.go
+++ cachetable.go
@@ -245,25 +245,25 @@ func (table *CacheTable) Flush() {
 	}
 }
 
-type Pair struct {
+type CacheItemPair struct {
 	Key interface{}
-	Value int64
+	AccessCount int64
 }
 
-// A slice of Pairs that implements sort.Interface to sort by Value.
-type PairList []Pair
-func (p PairList) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
-func (p PairList) Len() int { return len(p) }
-func (p PairList) Less(i, j int) bool { return p[i].Value > p[j].Value }
+// A slice of CacheIemPairs that implements sort. Interface to sort by AccessCount.
+type CacheItemPairList []CacheItemPair
+func (p CacheItemPairList) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
+func (p CacheItemPairList) Len() int { return len(p) }
+func (p CacheItemPairList) Less(i, j int) bool { return p[i].AccessCount > p[j].AccessCount }
 
 func (table *CacheTable) MostAccessed(count int64) []interface{} {
 	table.RLock()
 	defer table.RUnlock()
 
-	p := make(PairList, len(table.items))
+	p := make(CacheItemPairList, len(table.items))
 	i := 0
 	for k, v := range table.items {
-		p[i] = Pair{k, v.accessCount}
+		p[i] = CacheItemPair{k, v.accessCount}
 		i++
 	}
 	sort.Sort(p)
