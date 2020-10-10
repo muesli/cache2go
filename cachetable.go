096fe9c429c8933285bfe226ff72dc37788b3c0d
Lakshan Dissanayake <supunlakshan.xfinity@gmail.com>
2020-10-11 03:39:27 +0530

    fixed concurrent cache management issue

--- cachetable.go
+++ cachetable.go
@@ -48,9 +48,6 @@ func (table *CacheTable) Count() int {
 
 // Foreach all items
 func (table *CacheTable) Foreach(trans func(key interface{}, item *CacheItem)) {
-	table.RLock()
-	defer table.RUnlock()
-
 	for k, v := range table.items {
 		trans(k, v)
 	}
